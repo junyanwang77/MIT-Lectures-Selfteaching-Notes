e61c754119366bb74652da24ebe6573c6808b4a6
junyanwang77 <junyanwang77@gmail.com>
2019-07-17 15:42:07 +0800

    Create ps1a.py

---
+++ ps1a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 23:11:58 2019
+
+@author: apple
+"""
+
+input("Enter your annual salary: ")
+input("Enter the percent of your salary to save,as a decimal: ")
+input("Enter the cost of your dream home: ")
+total_cost = int(input("Enter the cost of your dream home: "))
+portion_down_payment = total_cost * 0.25
+current_savings = 0
+annual_salary = int(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save,as a decimal: "))
+input("Number of month: ")
