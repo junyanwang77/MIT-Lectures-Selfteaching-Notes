85fe39c6eb3a54a657aa434387c7b724a848adcb
junyanwang77 <junyanwang77@gmail.com>
2019-07-17 15:42:04 +0800

    Create ps0.py

---
+++ ps0.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 15:33:32 2019
+
+@author: apple
+"""
+
+import math
+
+input("Enter number x: ")
+input("Enter number y: ")
+x = 2
+y = 3
+print("x**y = ",x**y)
+print("log(x) = ",int(math.log(x,2)))
+
